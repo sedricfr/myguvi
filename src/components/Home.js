30585c56565154800b92259ec454d9f16d643689
Sedvin1903 <frsedric2003@gmail.com>
2023-06-09 19:53:54 +0530

    Hi2

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
-import React ,{ useEffect } from "react"
-import {useLocation, useNavigate} from 'react-router-dom';
+import React  from "react"
+import {useLocation} from 'react-router-dom';
 
 
 function Home (){
