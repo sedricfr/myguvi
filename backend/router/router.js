0249674dc492dd2af2240360c802f8caed57e9ee
sedricfr <67958943+sedricfr@users.noreply.github.com>
2023-06-21 23:11:26 +0530

    Profile i

--- backend/router/router.js
+++ backend/router/router.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../Controller/controller");
+const cors = require("cors")
+
 
 //=======================> User <====================
 
 router.post("/api/user/signup", userController.register);
 router.post("/api/user/login", userController.loginUser);
+router.get("api/user/${userId}",userController.fetchUserbio);
+router.post("/api/user/bio", userController.editProfile);
 router.post("/api/user/logout", userController.logout);
+
 
 
 module.exports = router;
